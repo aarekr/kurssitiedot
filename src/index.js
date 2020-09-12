0bc590a0efdb9130236641c6f88f71e860f31356
Aare <aare212@hotmail.com>
2020-09-12 20:32:06 +0300

    2.4: kurssitiedot step9

--- src/index.js
+++ src/index.js
@@ -1,82 +1,105 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Python web development',
-        exercises: 100,
-        id: 4
-      }
-    ]
-  }
+const App = (props) => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
 
   return (
     <div>
-      <Course course={course} />
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return(
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-const Header = ({ name }) => {
-  return(
-    <div>
-      <h1>{name}</h1>
-    </div>
-  )
-}
-
-const Content = ({ parts }) => {
-  return(
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />
+      <h1>Web development curriculum</h1>
+      {courses.map((name, i) =>
+        <Course key={i} course={courses[i].name} parts={courses[i].parts} />
       )}
     </div>
   )
 }
 
-const Part = ({ part }) => {
+const Course = (props) => {
   return(
     <div>
-      <p>{part.name} {part.exercises}</p>
+      <Header course={props.course} />
+      <Content parts={props.parts} />
+      <Total parts={props.parts} />
+    </div>
+  )
+}
+const Header = (props) => {
+  return(
+    <div>
+      <h2>{props.course}</h2>
     </div>
   )
 }
 
-const Total = ({ parts }) => {
-  console.log(parts)
+const Content = (props) => {
+  return(
+    <div>
+      {props.parts.map((name, i) =>
+        <Part key={i} part={props.parts[i].name} exercises={props.parts[i].exercises} />
+      )}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  
   let i = 0
   const taulu = [] // luodaan uusi tyhjä taulu
-  while(i < parts.length){
-    taulu.push(parts[i].exercises) // syötetään määrät tauluun
+  while(i < props.parts.length){
+    taulu.push(props.parts[i].exercises) // syötetään määrät tauluun
     i++
   }
   const reducer = (acc, curVal) => acc + curVal; // laskenta
